3ce645f092f0e14f8d01ccb10e019f3fd18773ae
Nick Miller <nick@nmiller.info>
2018-08-07 08:26:45 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :notes, param: :identifier, only: [:show, :new], format: false, path: '' do
+    resource :likes, only: :create, format: false, path: 'like'
+  end
+  resources :notes, only: [:create], format: false, path: 'new'
+
   root 'index#index'
 end
