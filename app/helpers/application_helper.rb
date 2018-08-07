3b368f2e7d290a25158227799a7536fb1ded8690
Nick Miller <nick@nmiller.info>
2018-08-07 08:50:43 -0400

    Add helper method for generating page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  # title defines content for :title within a view.
+  def title(t)
+    content_for(:title) { t + ' - ' }
+  end
+
   # hashed_ip returns a hexadecimal-encoded hash of the IP address of the
   # current request.
   def hashed_ip
