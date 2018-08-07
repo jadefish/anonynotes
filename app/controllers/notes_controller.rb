6e3329a2c6117c401efc2190da9902eaf8d5789e
Nick Miller <nick@nmiller.info>
2018-08-07 08:30:47 -0400

    Implement note creation

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,16 +1,30 @@
 class NotesController < ApplicationController
   # new handles GET /new.
   def new
-    # TODO
+    @note = Note.new
   end
 
   # create handles POST /new.
   def create
-    # TODO
+    @note = Note.create(note_params)
+    @note.likes.create(ip_hash: helpers.hashed_ip)
+
+    if @note.save
+      redirect_to @note
+    else
+      render 'new'
+    end
   end
 
   # show handles GET /:identifier.
   def show
     # TODO
   end
+
+  private
+    def note_params
+      params.require(:note).permit(:text).merge(
+        identifier: Note.generate_identifier
+      )
+    end
 end
