08bafaf2923c1385f3d120355cedde3e4c938bce
Nick Miller <nick@nmiller.info>
2018-08-07 08:24:12 -0400

    Add helper method for obtaining hashed request IP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # hashed_ip returns a hexadecimal-encoded hash of the IP address of the
+  # current request.
+  def hashed_ip
+    return HMAC.instance.hash request.remote_ip
+  end
 end
