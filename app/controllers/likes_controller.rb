d766a896b0501e97cdb85ca1ea060c89c9d89558
Nick Miller <nick@nmiller.info>
2018-08-09 07:12:43 -0400

    Remove redundant comment and block

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,7 @@ class LikesController < ApplicationController
   # create handles POST /:note_identifier/like.
   def create
     @note = Note.find_by identifier: params[:note_identifier]
-
-    # Since we're not XHRing, differentiate between liking and unliking notes
-    # by checking if the current user has already liked @note:
-    like = @note.likes.find_or_create_by ip_hash: helpers.hashed_ip
-
-    if !like
-      # Like the note:
-      @note.likes.create(ip_hash: helpers.hashed_ip)
-    end
+    @like = @note.likes.find_or_create_by ip_hash: helpers.hashed_ip
 
     redirect_to @note
   end
