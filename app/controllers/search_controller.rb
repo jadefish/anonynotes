22c16fc605f5042ccd689086e0fa135f03d00d82
Nick Miller <nick@nmiller.info>
2018-08-14 15:08:52 -0400

    Implement search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
+# SearchController handles search-related actions.
 class SearchController < ApplicationController
   def search
-    @results = []
     @query = request.query_parameters[:q]
+    @results = Note.search @query
   end
 end
